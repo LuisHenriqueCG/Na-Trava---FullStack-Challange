a50b3f056f78bc8e2cd5f8d13349a2fb645d3344
Luis Henrique de Camargo Gonçalves <luis.camargo@edu.unipar.br>
2022-10-10 18:46:31 -0300

    feat: create complete project

---
+++ setup.js
@@ -0,0 +1,13 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import { router} from './router.js'
+import cors from '@koa/cors';
+//common JS
+
+export const app = new Koa();
+
+app.use(cors());
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
